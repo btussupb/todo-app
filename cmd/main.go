233f84fa0311a013e559ff6f0f7d3be483b69276
Your Name <you@example.com>
2023-02-16 22:53:53 +0600

    router

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/btussupb/todo-app"
+	"github.com/btussupb/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running http server: %s", err.Error())
 	}
 }
